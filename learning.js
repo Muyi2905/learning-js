890389851dc197eca17ced32b21971d28d252bbd
Muyi2905 <obaremimuyiwa@gmail.com>
2022-11-19 16:00:49 -0600

    update

--- learning.js
+++ learning.js
@@ -115,5 +115,18 @@ function fizzBuzz() {
         i++
     }
 
-//do loop
-//it works like the while statement but the difference is that it checks for the condition after the do statement has run
+    //do loop
+    //it works like the while statement but the difference is that it checks for the condition after the do statement has run
+
+    var numberOfBottles = 99
+    while (numberOfBottles >= 0) {
+        var bottleWord = "bottle";
+        if (numberOfBottles === 1) {
+            bottleWord = "bottles";
+        }
+        console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+        console.log(numberOfBottles + " " + bottleWord + " of beer,");
+        console.log("Take one down, pass it around,");
+        numberOfBottles--;
+        console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+    }
